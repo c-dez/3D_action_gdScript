063583ea8cff1d58c43937e8cdb344cae216b477
c-dez <hdezcruzcarlos@gmail.com>
2024-05-17 00:56:58 -0600

    fixed shoot() behaving as wanted

--- raycast/ray_cast_shoot.gd
+++ raycast/ray_cast_shoot.gd
@@ -1,12 +1,35 @@
 extends RayCast3D
 # rotation and position controlled by CamMount
 
+var timer: float = 0.3
+var internal_timer: float = 0
 
 
-func _process(_delta):
-	if is_colliding():
-		print(get_collider().get_class())
+
+func _physics_process(_delta):
+	# if get_collider() is CharacterBody3D:
+	# 	if get_collider().has_method("change_size") and Input.is_action_just_pressed("fire1"):
+	# 		get_collider().change_size()
 		
-	pass
+	shoot(_delta)		
+		
 
 
+func shoot(_delta):
+	if Input.is_action_just_pressed("fire1") and internal_timer == 0:
+		internal_timer = timer
+
+	if internal_timer > 0:
+		internal_timer -= _delta
+		if get_collider() is CharacterBody3D:
+			get_collider().change_size()
+	elif internal_timer < 0:
+		internal_timer = 0
+
+		# cuando disparo quiero que
+		# durante timer segundos
+		# obtenga el collider
+		# e invoque sus methodos
+
+
+		
