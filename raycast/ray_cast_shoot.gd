77083a1d54056fbad0d8daf38cca92e8fb0d1a98
c-dez <hdezcruzcarlos@gmail.com>
2024-05-17 15:04:22 -0600

    testing successful, need to expand npc class

--- raycast/ray_cast_shoot.gd
+++ raycast/ray_cast_shoot.gd
@@ -2,34 +2,31 @@ extends RayCast3D
 # rotation and position controlled by CamMount
 
 var timer: float = 0.3
-var internal_timer: float = 0
+var temporal_reference: CharacterBody3D
+var internal_timer: float = 0.0
+
+var gun_damage: int = 1
 
 
-
-func _physics_process(_delta):
-	# if get_collider() is CharacterBody3D:
-	# 	if get_collider().has_method("change_size") and Input.is_action_just_pressed("fire1"):
-	# 		get_collider().change_size()
-		
+func _physics_process(_delta):	
 	shoot(_delta)		
 		
 
 
-func shoot(_delta):
-	if Input.is_action_just_pressed("fire1") and internal_timer == 0:
-		internal_timer = timer
+func shoot(_delta)-> void:
 
+	if Input.is_action_just_pressed("fire1") and internal_timer == 0.0:
+		internal_timer = timer
 	if internal_timer > 0:
 		internal_timer -= _delta
 		if get_collider() is CharacterBody3D:
-			get_collider().change_size()
-	elif internal_timer < 0:
+			temporal_reference = get_collider()
+			
+	elif internal_timer <= 0:
 		internal_timer = 0
-
-		# cuando disparo quiero que
-		# durante timer segundos
-		# obtenga el collider
-		# e invoque sus methodos
-
-
+		temporal_reference = null
 		
+	if temporal_reference != null and Input.is_action_just_released("fire1")  :
+		temporal_reference.test_shoot(gun_damage)
+		
+		pass
