1777aa5aa259328a071f77b09dbb623a75519ba6
c-dez <hdezcruzcarlos@gmail.com>
2024-05-16 19:21:00 -0600

    starting shooting

--- cross_hair/CrossHair.gd
+++ cross_hair/CrossHair.gd
@@ -4,7 +4,9 @@ var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport().get_size()/2
-	position = screen_size
+	position.x = screen_size.x -20
+	position.y = screen_size.y -20
+
 	pass # Replace with function body.
 
 
