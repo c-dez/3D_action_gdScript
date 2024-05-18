c9b86069dd59cbbc67543e4998d3eff0034972b2
c-dez <hdezcruzcarlos@gmail.com>
2024-05-17 21:26:24 -0600

    fixed bugs

--- raycast/ray_cast_shoot.gd
+++ raycast/ray_cast_shoot.gd
@@ -4,17 +4,16 @@ extends RayCast3D
 var timer: float = 0.3
 var temporal_reference: CharacterBody3D
 var internal_timer: float = 0.0
-
+# test
 var gun_damage: int = 1
 
 
-func _physics_process(_delta):	
+func _process(_delta):	
 	shoot(_delta)		
 		
 
-
+# testing
 func shoot(_delta)-> void:
-
 	if Input.is_action_just_pressed("fire1") and internal_timer == 0.0:
 		internal_timer = timer
 	if internal_timer > 0:
@@ -26,7 +25,6 @@ func shoot(_delta)-> void:
 		internal_timer = 0
 		temporal_reference = null
 		
-	if temporal_reference != null and Input.is_action_just_released("fire1")  :
+	if temporal_reference != null and Input.is_action_just_pressed("fire1"):
 		temporal_reference.test_shoot(gun_damage)
 		
-		pass
